4daf49414f1f5eda62dc55480733e3e7ad5f4284
chip <onlygamernerd@gmail.com>
2020-10-13 21:49:01 +0200

    Tweak(FeedItem): Removal of reply secton. Adding later

--- client/src/components/Feed/components/FeedItem.tsx
+++ client/src/components/Feed/components/FeedItem.tsx
@@ -5,14 +5,14 @@ import Paper from '@material-ui/core/Paper';
 const useStyles = makeStyles((theme: Theme) => 
   createStyles({
     paper: {
-      margin: 10,
+      marginTop: 30,
       background: '#232323',
-      color: '#fff'
+      color: '#fff' 
     },
     header: { 
       color: '#fff'
     },
-    replies: {
+    content: {
       marginLeft: 30
     }
   }),
@@ -21,15 +21,15 @@ const useStyles = makeStyles((theme: Theme) =>
 export const FeedItem = (feed) => {
   const classes = useStyles();
   return (
-    <Paper className={classes.paper} variant="outlined">
-      <div className={classes.header}>
-        <h2>{feed.name}</h2>
-      </div>
-      <p>{feed.content}</p>
-      <div className={classes.replies}>
-        <h2>{feed.reply.name}</h2>
-        <p>{feed.reply.content}</p>
-      </div>
-    </Paper>
+    <div>
+      <Paper className={classes.paper} variant="outlined">
+        <div className={classes.content}>
+          <div className={classes.header}>
+          <h2>{feed.name}</h2>
+          </div>
+          <p>{feed.content}</p>
+        </div>
+      </Paper>
+    </div>
   )
 }
