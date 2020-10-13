ac1cd8a4683b88a06f66bcba21e13980b36adc6c
chip <onlygamernerd@gmail.com>
2020-10-13 21:16:40 +0200

    Feat(Feed): Fetching posts

--- client/src/components/Feed/components/FeedContainer.tsx
+++ client/src/components/Feed/components/FeedContainer.tsx
@@ -5,7 +5,16 @@ import { FeedList } from './FeedList';
 import { useFeed } from '../hooks/useFeed';
 
 export const FeedContainer = () => {
-  const { feedList } = useFeed()
+  const { feedList, setFeedList } = useFeed()
+
+  const getPostClient = () => {
+    fetch('http://localhost:5000/posts')
+      .then(res => res.json())
+      .then(response => console.log(response))
+  }
+
+  getPostClient() 
+
   return (
     <Container>
       <FeedList content={feedList} />
