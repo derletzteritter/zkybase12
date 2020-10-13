b8050e5d5d90dcaed5b62c33136d5ccf5f69ac95
Christopher <gjeltendigital@gmail.com>
2020-10-13 14:48:01 +0200

    Hardcoded data for testing

--- client/src/components/Feed/hooks/useFeed.ts
+++ client/src/components/Feed/hooks/useFeed.ts
@@ -2,7 +2,19 @@ import { atom, useRecoilState } from 'recoil';
 
 export const feedListState = atom({
   key: 'feedListState',
-  default: []
+  default: [
+    {
+      id: 1,
+      name: 'Christopher Gjelten',
+      content: 'Har noen tid til å dra ut og handle litt mat for meg?',
+      reply: {
+        id: 1,
+        name: 'Ola havlorsen',
+        content: 'Seff, det fikser jeg!'
+      }
+      
+    }
+  ]
 })
 
 export const useFeed = () => {
