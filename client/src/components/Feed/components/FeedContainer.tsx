546e30bccd212e7bd51dc730d8108f976f9fbf36
Christopher <gjeltendigital@gmail.com>
2020-10-13 14:50:24 +0200

    FeedContainer component

--- client/src/components/Feed/components/FeedContainer.tsx
+++ client/src/components/Feed/components/FeedContainer.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
 import Container from '@material-ui/core/Container';
+import { FeedList } from './FeedList';
+
+import { useFeed } from '../hooks/useFeed';
 
 export const FeedContainer = () => {
+  const { feedList } = useFeed()
   return (
     <Container>
+      <FeedList content={feedList} />
     </Container>
   )
 }
