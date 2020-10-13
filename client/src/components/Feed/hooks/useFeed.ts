b0f46c858664df494b629a41c9da065e1cc53941
chip <onlygamernerd@gmail.com>
2020-10-13 21:16:07 +0200

    Tweak(useFeed): Deletion of mock data

--- client/src/components/Feed/hooks/useFeed.ts
+++ client/src/components/Feed/hooks/useFeed.ts
@@ -2,19 +2,7 @@ import { atom, useRecoilState } from 'recoil';
 
 export const feedListState = atom({
   key: 'feedListState',
-  default: [
-    {
-      id: 1,
-      name: 'Christopher Gjelten',
-      content: 'Har noen tid til å dra ut og handle litt mat for meg?',
-      reply: {
-        id: 1,
-        name: 'Ola havlorsen',
-        content: 'Seff, det fikser jeg!'
-      }
-      
-    }
-  ]
+  default: []
 })
 
 export const useFeed = () => {
