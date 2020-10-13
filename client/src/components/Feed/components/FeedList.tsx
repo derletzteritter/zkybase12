fa8bee3f2a53a35ddfe6709236af2d26f27c5126
Christopher <gjeltendigital@gmail.com>
2020-10-13 14:49:35 +0200

    Created FeedList component

--- client/src/components/Feed/components/FeedList.tsx
+++ client/src/components/Feed/components/FeedList.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FeedItem } from './FeedItem'
 
 export const FeedList = ({ content }) => {
   return (
     <div>
-      
+      {content.map((con) => (
+        <FeedItem key={con.id} {...con}/>
+      ))}
     </div>
   )
 }
