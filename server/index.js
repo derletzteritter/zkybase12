966433d0c7a8ae8e85d7f15b88b640a1c9901582
chip <onlygamernerd@gmail.com>
2020-10-13 20:36:22 +0200

    Feat(SQL): Start on MySQL implentation

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+var mysql = require('mysql');
+
 const app = express();
 
 const PORT = 5000;
@@ -9,4 +11,42 @@ app.listen(PORT, () => {
 
 app.get('/', (req, res) => {
   res.send('Server is running!')
+})
+
+
+// Database functions
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "covid"
+})
+
+// Check forc connection
+
+con.connect(async (err) => {
+  if (err) throw err;
+  console.log('Connected');
+  app.get('/connection', (req, res) => {
+    res.send('Connected to the database')
+  })
+})
+
+function getPost(callback) {
+  con.query("SELECT * FROM posts", (err, result, fields) => {
+    if (err) throw err;
+    callback(result)
+  })
+}
+
+function setPost(callback) {
+  // Sets the post to the databas
+}
+
+
+getPost((result) => {
+  app.get('/posts', (req, res) => {
+    res.send(result);
+  })
 })
