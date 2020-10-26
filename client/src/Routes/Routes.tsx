07fac407570caed6957265fad47173eed4561a20
chip <onlygamernerd@gmail.com>
2020-10-26 15:21:02 +0100

    feat(routes): new route to posting

--- client/src/Routes/Routes.tsx
+++ client/src/Routes/Routes.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { FeedContainer } from '../components/Feed/components/FeedContainer'
 import { HeaderContainer } from '../components/Header/HeaderContainer'
 import { HomeContainer } from '../components/Home/components/HomeContainer';
+import { PostContainer } from '../components/Post/components/PostContainer';
 
 export const Routes = () => {
   return (
@@ -14,6 +15,7 @@ export const Routes = () => {
         <Switch>
             <Route path="/" exact component={HomeContainer} /> 
             <Route path="/feed" component={FeedContainer}/>
+            <Route path="/post" component={PostContainer} />
         </Switch>
       </div>
     </Router>
