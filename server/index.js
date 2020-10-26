ac4bd2c3a83d6edcbdf8f1c01ece303ee2d03e95
chip <onlygamernerd@gmail.com>
2020-10-26 15:22:52 +0100

    feat(backend): taking post request | sql inserts | api stuff

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 var mysql = require('mysql');
+const e = require('express');
+var bodyParser = require('body-parser')
 
 const app = express();
 
 const PORT = 5000;
 
 app.use(cors())
+app.use(bodyParser.json())
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`)
@@ -15,7 +18,6 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.send('Server is running!')
 })
-
 
 // Database functions
 
@@ -26,8 +28,7 @@ var con = mysql.createConnection({
   database: "covid"
 })
 
-// Check forc connection
-
+// Check for connection
 con.connect((err) => {
   if (err) throw err;
   console.log('Connected');
@@ -36,8 +37,8 @@ con.connect((err) => {
   })
 })
 
-function getPost(callback) {
-  con.query("SELECT * FROM posts", (err, result, fields) => {
+async function getPost(callback) {
+  con.query("SELECT * FROM posts ORDER BY id DESC", (err, result, fields) => {
     if (err) throw err;
     callback(result)
   })
@@ -45,13 +46,34 @@ function getPost(callback) {
 
 
 
-function setPost(callback) {
+function setPost(name, content) {
   // Sets the post to the database
+  const query = "INSERT INTO posts (name, content) VALUES (?, ?) "
+  const args = [name, content]
+  con.query(query, args, (err, result, fields) => {
+    if (err) throw err;
+    console.log(result);
+  })
+  getPost((result) => {
+    app.get('/posts', (req, res) => {
+      res.send(result);
+    })
+  })
 }
 
 
 getPost((result) => {
   app.get('/posts', (req, res) => {
     res.send(result);
+  })
+})
+
+app.post('/posts', (req, res) => {
+  res.send('Someone sent a response')
+  setPost(req.body.title, req.body.message);
+  getPost((result) => {
+    app.get('/posts', (req, res) => {
+      res.send(result);
+    })
   })
 })
