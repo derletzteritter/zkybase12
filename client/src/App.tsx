00916666be9cc24b2c60db6a8a84b8daacc310e4
itschip <onlygamernerd@gmail.com>
2020-11-18 14:48:49 +0100

    check if user is logged in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Auth } from './components/Auth/Auth';
+import { useAuth } from './components/Auth/hooks/useAuth';
 import { Routes } from './Routes/Routes';
 
 function App() {
+  const { user } = useAuth();
   return (
-    <Routes />
+    <div>
+      {!user ? (<Auth />) : (<Routes />)}
+    </div>
   );
 }
 
